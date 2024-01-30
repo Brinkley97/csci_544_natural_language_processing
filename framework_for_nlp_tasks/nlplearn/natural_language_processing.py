9f7ebe202984ff877422d57a29c3c0a88382723b
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-01-29 19:05:02 -0800

    going through tutorial for tf-idf

--- framework_for_nlp_tasks/nlplearn/natural_language_processing.py
+++ framework_for_nlp_tasks/nlplearn/natural_language_processing.py
@@ -1,8 +1,13 @@
-from __future__ import annotations
 import numpy as np
 import pandas as pd
 
+from scipy.spatial import distance
 from abc import ABC, abstractmethod
+from sklearn.metrics import pairwise_distances
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import euclidean_distances, cosine_similarity
+
+
 
 class NLP_General(ABC):
     def __init__(self, **kwargs):
@@ -12,6 +17,7 @@ class NLP_General(ABC):
         pass
 
 class NLP_File(NLP_General):
+    """Build a nlp object from a file. Will later distinguish between other types of datasources (ie: webpage, audio, etc)."""
 
     __name__ = 'NLP_File'
 
@@ -20,10 +26,54 @@ class NLP_File(NLP_General):
         self.file_path = file_path
         self.dataframe = dataframe
 
-    def display_dataframe(self):
+    def display_dataframe(self) -> pd.DataFrame:
         if self.dataframe is not None:
             print("DataFrame loaded from file:")
             return self.dataframe
         else:
             print("No DataFrame loaded.")
     
+    ## Euclidean Distance
+    
+    def euclidean_function(self, vectors):
+        euclideans = []
+        euc=euclidean_distances(vectors[0], vectors[1])
+        euclideans.append(euc)
+
+        return euclideans
+
+    
+    def convert(self, dist):
+        new_df = pd.DataFrame()
+        arr = np.array(dist)
+        arr = np.concatenate(arr, axis=0)
+        arr = np.concatenate(arr, axis=0)
+        
+        new_df['euclidean'] = arr
+
+        return new_df
+    def tfidf(self, string1: str, string2: str):
+        """"TF-IDF"""
+        ques = []
+
+        x = self.display_dataframe().iloc[:, 1:5]
+        x = x.dropna(how = 'any')
+        
+        for k in range(len(x)):
+            for j in [2, 3]:
+                ques.append(x.iloc[k, j])
+        vect = TfidfVectorizer()
+        # Fit the your whole dataset. After all, this'll 
+        # produce the vectors which is based on words in corpus/dataset
+        vect.fit(ques)
+    
+        corpus = [string1, string2]
+        trans = vect.transform(corpus)
+    
+        edu = self.euclidean_function(trans)
+        edu_score = self.convert(edu)
+        # self.cosine(trans)
+        # self.manhatten_distance(trans)
+
+        return edu_score
+
